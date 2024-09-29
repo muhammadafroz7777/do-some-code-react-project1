1a4a5faef9d80e20f4c7673f3cd49d457481d3b9
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-09-30 00:05:03 +0500

    22:34

--- project--1/src/App.jsx
+++ project--1/src/App.jsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        hello world 
- 
-      </div>
-     
-    </>
-  )
+
+    return <div>
+
+
+My First Component
+
+    </div>;
+
 }
 
-export default App
+export default App;
