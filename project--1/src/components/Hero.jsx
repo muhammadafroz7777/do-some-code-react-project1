333979bd8bbf32d54127b87dce188dc34d45168a
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-11-04 16:26:56 +0500

    45:13.

---
+++ project--1/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+const HeroSection = () => {
+
+
+return (
+    <main className="hero"> 
+    <div className="hero-content"></div>
+    <div className="hero-image"></div>
+
+    </main>
+);  
+}
+
+
+export default HeroSection;
