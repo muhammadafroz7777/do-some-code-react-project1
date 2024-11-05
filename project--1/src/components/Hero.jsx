06f8378d4495817e1845d48f763c9080262a2653
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-11-05 16:07:06 +0500

    conti

--- project--1/src/components/Hero.jsx
+++ project--1/src/components/Hero.jsx
@@ -3,8 +3,23 @@ const HeroSection = () => {
 
 return (
     <main className="hero"> 
-    <div className="hero-content"></div>
-    <div className="hero-image"></div>
+    <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <div className="hero-btn">
+            <button>Shop Now</button>
+            <button>Category</button>
+        </div>
+        <div className='shopping'>
+            <p>Also Available On</p>
+            <div className='brand-icons'>
+                <img src="/images/amazon.png" alt="amazon-logo" />
+                <img src="/images/flipkart.png" alt="amazon-logo" />
+            </div>
+        </div>
+    </div>
+
+    <div className="hero-image "></div>
 
     </main>
 );  
