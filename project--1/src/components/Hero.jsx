2eb6d49781dbed389f5c1530e2a80ab21a2e17eb
muhammadafroz7777 <muhammadafroz0320@gmail.com>
2024-11-06 16:30:07 +0500

    do some code project-1 done

--- project--1/src/components/Hero.jsx
+++ project--1/src/components/Hero.jsx
@@ -8,7 +8,7 @@ return (
         <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
         <div className="hero-btn">
             <button>Shop Now</button>
-            <button>Category</button>
+            <button className="secondary-btn">Category</button>
         </div>
         <div className='shopping'>
             <p>Also Available On</p>
